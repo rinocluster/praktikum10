bd91e38585b5b70ec969356685b2d24db2f2b5c6
rinocluster <38765721+rinocluster@users.noreply.github.com>
2018-12-22 15:39:13 +0700

    Update 1.py
    Update 1.py

--- 1.py
+++ 1.py
@@ -1,44 +1,31 @@
-nilai = 50
-
-if nilai = 75: # equal eksplisit
-    print("nilai anda:",nilai)
-
-if nilai is 60: # equal
-    print("nilai anda:")
-
-if nilai is not 60: # not equal
-    print("nilai anda bukan: 60")
-
-print(100*"=")
-
-nilai = 65
-
-if 80 < nilai < 100
-    print("nilai anda adalah A")
-elif 70 < nilai < 80:
-    print("nilai anda adalah B")
-elif 60 < nilai < 70:
-    print("nilai anda adalah C")
-elif 50 < nilai < 60:
-    print("nilai anda adalah T, lakukan perbaikan")
-else
-    print("maaf anda tidak lulus mata kuliah ini")
-
-print(100*"+")
-print("operator logika untuk list dan string")
-print(" ")
-
-gorengan=["bakwan","cireng","bala-bala","gehu","combro","pisang goreng","pukis","risoles"]
-beli = "sepatu"
-
-if beli in gorengan:
-    print('Mamang bilang, " ya, saya jual',beli,'"')
-
-if not beli in gorengan:
-    print('"saya gak jual',beli,'"')
-
-karakter = "z"
-if karakter in beli:
-    print("ada", karakter, "di",beli)
-else:
-    print("tidak ada",karakter,"di",beli)
+@@ -1,6 +1,9 @@
+ nilai = 50	 #Nama : Rino Arya Kusuma
+ #Nim : G.231.16.0135
+ 
+  
+  if nilai = 75: # equal eksplisit	 nilai = 75
+ 
+  if nilai == 75: # equal eksplisit
+     print("nilai anda:",nilai)	     print("nilai anda:",nilai)
+ 
+  
+  if nilai is 60: # equal	 if nilai is 60: # equal
+@@ -13,15 +16,15 @@
+ 
+  
+  nilai = 65	 nilai = 65
+ 
+  
+  if 80 < nilai < 100	 if 80 < nilai < 100:
+     print("nilai anda adalah A")	     print("nilai anda adalah A")
+ elif 70 < nilai < 80:	 elif 70 < nilai < 80:
+     print("nilai anda adalah B")	     print("nilai anda adalah B")
+ elif 60 < nilai < 70:	 elif 60 < nilai < 70:
+     print("nilai anda adalah C")	     print("nilai anda adalah C")
+ elif 50 < nilai < 60:	 elif 50 < nilai < 60:
+     print("nilai anda adalah T, lakukan perbaikan")	     print("nilai anda adalah T, lakukan perbaikan")
+ else	 else:
+     print("maaf anda tidak lulus mata kuliah ini")	     print("maaf anda tidak lulus mata kuliah ini")
+ 
+  
+  print(100*"+")	 print(100*"+")
